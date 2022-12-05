d03262745402b0a1ac8a0c70545760d19dd6c2a9
gamingflexer <osurve20ece@student.mes.ac.in>
2022-12-05 19:02:31 +0530

    collab scripts improved

--- collab/end.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-book_no = sys.argv[1]
-
-from google.colab import files
-files.download(f"/content/bhme/data/done/output_en_hi_{book_no}_.csv")
--- collab/start.py
+++ collab/start.py
@@ -8,6 +8,7 @@ path_to_download_corpus = "/content/data"
 
 import gdown
 gdown.download_folder(link, quiet=True,output=path_to_download_corpus,remaining_ok=True)
+print('Starting...\n')
 
 import os
 def get_txt_file(number_file,path):
@@ -22,6 +23,8 @@ file_to_open = get_txt_file(book_no,path_to_download_corpus)
 
 os.system(f"mkdir /content/bhme/data")
 os.system(f"mv /content/data/chunksmr_{book_no}_.txt /content/bhme/data/")
+
+print('Task Started...\n next task will take time')
 
 os.system("python /content/bhme/dataset_locally.py")
 
